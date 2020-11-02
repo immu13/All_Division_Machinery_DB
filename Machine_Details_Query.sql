e7559f1d4d9027f969ba31a47f21f22d42b06f33
immu13 <71899855+immu13@users.noreply.github.com>
2020-11-02 07:56:06 +0530

    Add files via upload

--- Machine_Details_Query.sql
+++ Machine_Details_Query.sql
@@ -2,7 +2,7 @@
 --create database Mac_Details
 
 create table Machine_Details (Division varchar(3), Machine_Name varchar(50), Capacity varchar(10), Machine_Code varchar(10),
-No_of_Machines numeric(3), Serial_No numeric(5), Purchased_Date Date, Supplier_Name varchar(50), Final_Machine_Code varchar(30),
+No_of_Machines numeric(3), Serial_No numeric(5), Final_Machine_Code varchar(30), Purchased_Date Date, Supplier_Name varchar(50), 
 Machine_Id int primary key identity (1,1))
 
 select * from Machine_Details
@@ -10,4 +10,6 @@ select * from Machine_Details
 select @@SERVERNAME
 
 select Machine_Name, Division from Machine_Details
+
+drop table Machine_Details
 
